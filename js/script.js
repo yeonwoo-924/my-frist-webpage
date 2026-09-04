212c85be53c72472b4cc29c597a2896c4739403e
yeonwoo-924 <gangy504@gmail.com>
2026-09-04 16:54:48 +0900

    feat: 히어로 슬라이드 영역 스와이퍼 라이브러리 작업중

--- js/script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("h1").addEventListener("click", function () {
-  alert("안녕하냐옹 다시 정상인인 척을해요");
-});
---
+++ js/swHero.js
@@ -0,0 +1,10 @@
+const swHero = new Swiper(".sw-Hero", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
